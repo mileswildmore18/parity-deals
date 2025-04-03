3ac5a5536f3ee81a10504ce73dcc187e260e9c15
Miles Wildmore <mileswildmore18@gmail.com>
2025-04-03 15:39:52 -0400

    Added the logo of the website along with a Navbar with clickable navigation buttons.

---
+++ src/app/(marketing)/_components/NavBar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import {BrandLogo} from "@/components/BrandLogo";
+
+export function NavBar() {
+    return <header className=" flex py-6 shadow-xl fixed top-0 w-full z-10 bg-background/95">
+        <nav className="flex items-center gap-10 container font-semibold">
+            <Link href="/" className="mr-auto">
+                <BrandLogo />
+            </Link>
+            <Link className="text-lg" href="#">
+                Features
+            </Link>
+            <Link className="text-lg" href="/#pricing">
+                Pricing
+            </Link>
+            <Link className="text-lg" href="#">
+                About
+            </Link>
+        </nav>
+    </header>
+}
